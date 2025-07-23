df2b300e1969a15f03ca788ca988dc30198b60e7
laleoarrow <luao@stu.cqmu.edu.cn>
2025-07-23 22:29:45 +0800

    20250723: adding get_meta_colnames function

---
+++ R/metadata.R
@@ -0,0 +1,15 @@
+#' get colnames of srt obj meta data name
+#'
+#' @param srt Seurat object
+#' @param pattern Pattern to match in column names, default is "RNA_snn_res"
+#'
+#' @returns A vector of column names that match the pattern
+#' @export
+#'
+#' @examples
+#' tnk %>% get_meta_colnames()
+get_meta_colnames <- function(srt, pattern = "RNA_snn_res") {
+  res_cols <- grep("^RNA_snn_res\\.", colnames(tnk@meta.data), value = TRUE) 
+  if (pattern == "RNA_snn_res") res_cols <- res_cols |> (\(x) x[order(as.numeric(sub("^RNA_snn_res\\.", "", x)))])()
+  return(res_cols)
+}
